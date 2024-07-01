be6720b171ae248e628ea0fac6647e2e3521292f
Amith Biju <amithbiju1@gmail.com>
2024-07-01 22:30:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./Components/Navbar";
+import { Home } from "./Pages/Home";
+import { Category } from "./Pages/Category";
+import { Fotter } from "./Components/Fotter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category" element={<Category />} />
+        </Routes>
+        <Fotter />
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Fotter.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Fotter = () => {
+  return (
+    <div>
+      <h1>Fotter</h1>
+    </div>
+  );
+};
