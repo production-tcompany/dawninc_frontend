e1b2b42c32478fc49ca52d6558bc6c4e4d93ad53
AlanSabuv8 <alansabuv8@gmail.com>
2024-07-03 12:43:06 +0530

    login_signin_navfix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,14 +2,17 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { CiHeart } from "react-icons/ci";
 import { BsCart, BsFillTriangleFill } from "react-icons/bs";
+import { FaRegCircle } from 'react-icons/fa';
 
 export const Navbar = () => {
   const location = useLocation();
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isSidebarDropdownOpen, setIsSidebarDropdownOpen] = useState(false);
+  const sidebarRef = useRef();
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
   };
 
   const handleClickOutside = (event) => {
@@ -36,14 +39,14 @@ export const Navbar = () => {
     <>
       {/* Mobile View */}
       <nav className="absolute top-0 left-0 right-0 z-10 flex items-center justify-between px-4 py-2 md:hidden">
-        <button onClick={toggleSidebar} className="text-white font-light">
+        <button onClick={toggleSidebar}>
           <FaRegCircle className={`w-6 h-6 ${textColor}`} />
         </button>
         <Link to="/" className="flex justify-center">
           <img src="https://s3-alpha-sig.figma.com/img/cb9f/6ae9/78c2aa35bcf4dae3731048b842aca7d3?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cjo5h3NHV~lhJ7hsz3Wmb2ghy9aZ3dCHxN3PrIZ-AQpB0l5SHFaZ6KN4ucFXNjGuvYTu7T-wD4x6OQL9KtOXhZ1mpIlkmBY6uthw3UaT2rytAEcGuJLoEFCFVaJ76UscKhcGPcV1BJdXv8e-EutYn2PTMemweULpD8GvJ5K7K1sHVXmCehFGl62j8JZ-C3az4jmQt6rBCCOYs3Ht93Urf8z9X8d1Ifg4TIYJsEuNTLYFmi0c7YRmKX4BtvFVaQhDeZEl0rimm~NaPQgR7LXOLM3vrJxylXiXsIa8XPKAat16D63Ou-VEr-qVOfMZZOjJQFZj7EMn45f8oR0Zm~b7bA__" alt="Logo" className="h-24" />
         </Link>
-        <Link to="/">
-          <BsCart className={`w-6 h-6 ${textColor}`} />
+        <Link to="/login">
+          <BsCart className= {`w-6 h-6 ${textColor}`} />
         </Link>
       </nav>
 
@@ -52,22 +55,40 @@ export const Navbar = () => {
         <ul className="flex flex-col items-center space-y-8 mt-8 py-20">
           <li><Link to="/" className="text-black font-light">Home</Link></li>
           <li className="flex justify-evenly space-x-4 w-full px-4">
-            <Link to="/" className="text-black font-light">Account</Link>
-            <Link to="/" className="text-black font-light">Wishlist</Link>
+            <Link to="/login" className="text-black font-light">Account</Link>
+            <Link to="/login" className="text-black font-light">Wishlist</Link>
           </li>
-          <li className="relative">
-            <button onClick={toggleDropdown} className="flex items-center text-black font-light">
+          <li
+            className="relative"
+            onMouseEnter={() => setIsSidebarDropdownOpen(true)}
+            onMouseLeave={(e) => {
+              if (!e.currentTarget.contains(e.relatedTarget)) {
+                setIsSidebarDropdownOpen(false);
+              }
+            }}
+          >
+            <button className="flex items-center text-black font-light">
               Collections
               <BsFillTriangleFill
-                className={`ml-2 md:ml-4 mt-1 transform transition-transform w-2 h-2 ${isDropdownOpen ? 'rotate-90' : 'rotate-180'}`}
+                className={`ml-2 md:ml-4 mt-1 transform transition-transform w-2 h-2 ${isSidebarDropdownOpen ? 'rotate-90' : 'rotate-180'}`}
               />
             </button>
-            {isDropdownOpen && (
-              <div className="absolute left-0 flex flex-col space-y-2 mt-2 p-4 bg-white">
-                <Link to="/collections" className="text-black font-light">Shirts</Link>
-                <Link to="/collections" className="text-black font-light">T-shirts</Link>
-                <Link to="/collections" className="text-black font-light">Hoodies</Link>
-                <Link to="/collections" className="text-black font-light">Pants</Link>
+            {isSidebarDropdownOpen && (
+              <div
+                className="absolute left-0 flex flex-col space-y-2 mt-2 p-4 bg-white"
+                onMouseEnter={() => setIsSidebarDropdownOpen(true)}
+                onMouseLeave={(e) => {
+                  if (!e.currentTarget.contains(e.relatedTarget)) {
+                    setTimeout(() => {
+                      setIsSidebarDropdownOpen(false);
+                    }, 1000);
+                  }
+                }}
+              >
+                <Link to="/collections/" className="text-black font-light">Shirts</Link>
+                <Link to="/collections/" className="text-black font-light">T-shirts</Link>
+                <Link to="/collections/" className="text-black font-light">Hoodies</Link>
+                <Link to="/collections/" className="text-black font-light">Pants</Link>
               </div>
             )}
           </li>
@@ -75,29 +96,47 @@ export const Navbar = () => {
       </div>
 
       {/* Larger Screens View */}
-      <nav className="hidden md:flex absolute top-0 left-0 right-0 z-10 items-center justify-between px-4 py-4 md:px-12 md:py-0">
+      <nav className="hidden md:flex fixed top-0 left-0 right-0 z-10 items-center justify-between px-4 py-4 md:px-12 md:py-8">
         <div className="flex items-center space-x-4 md:space-x-24">
           <Link to="/" className={`font-light ${textColor}`}>Home</Link>
-          <div className="relative group">
-            <button onClick={toggleDropdown} className={`flex items-center font-light ${textColor}`}>
+          <div
+            className="relative group"
+            onMouseEnter={() => setIsDropdownOpen(true)}
+            onMouseLeave={(e) => {
+              if (!e.currentTarget.contains(e.relatedTarget)) {
+                setTimeout(() => {
+                  setIsDropdownOpen(false);
+                }, 1000);
+              }
+            }}
+          >
+            <button className={`flex items-center font-light ${textColor}`}>
               Collections
               <BsFillTriangleFill
                 className={`ml-2 md:ml-4 mt-1 transform transition-transform w-2 h-2 ${isDropdownOpen ? 'rotate-90' : 'rotate-180'}`}
               />
             </button>
             {isDropdownOpen && (
-              <div className={`absolute left-0 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6 mt-2 p-4 whitespace-nowrap font-light ${textColor}`}>
-                <Link to="/collections" >Shirts</Link>
-                <Link to="/collections" >T-shirts</Link>
-                <Link to="/collections" >Hoodies</Link>
-                <Link to="/collections" >Pants</Link>
+              <div
+                className={`absolute left-0 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6 mt-2 p-4 whitespace-nowrap font-light ${textColor}`}
+                onMouseEnter={() => setIsDropdownOpen(true)}
+                onMouseLeave={(e) => {
+                  if (!e.currentTarget.contains(e.relatedTarget)) {
+                    setIsDropdownOpen(false);
+                  }
+                }}
+              >
+                <Link to="/collections/">Shirts</Link>
+                <Link to="/collections/">T-shirts</Link>
+                <Link to="/collections/">Hoodies</Link>
+                <Link to="/collections/">Pants</Link>
               </div>
             )}
           </div>
-          <Link to="/account" className={`font-light ${textColor}`}>Account</Link>
+          <Link to="/login" className={`font-light ${textColor}`}>Account</Link>
         </div>
 
-        <div className="flex justify-center">
+        <div className="flex justify-center absolute left-1/2 transform -translate-x-1/2">
           <img src="https://s3-alpha-sig.figma.com/img/cb9f/6ae9/78c2aa35bcf4dae3731048b842aca7d3?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cjo5h3NHV~lhJ7hsz3Wmb2ghy9aZ3dCHxN3PrIZ-AQpB0l5SHFaZ6KN4ucFXNjGuvYTu7T-wD4x6OQL9KtOXhZ1mpIlkmBY6uthw3UaT2rytAEcGuJLoEFCFVaJ76UscKhcGPcV1BJdXv8e-EutYn2PTMemweULpD8GvJ5K7K1sHVXmCehFGl62j8JZ-C3az4jmQt6rBCCOYs3Ht93Urf8z9X8d1Ifg4TIYJsEuNTLYFmi0c7YRmKX4BtvFVaQhDeZEl0rimm~NaPQgR7LXOLM3vrJxylXiXsIa8XPKAat16D63Ou-VEr-qVOfMZZOjJQFZj7EMn45f8oR0Zm~b7bA__" alt="Logo" className="h-10 md:h-28" />
         </div>
 
