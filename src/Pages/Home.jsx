7622e8bcc1dabc99254073a1b79f710b1f9d1377
AlanSabuv8 <alansabuv8@gmail.com>
2024-07-09 20:37:18 +0530

    frontend_update_4

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -14,7 +14,9 @@ export const Home = () => {
           muted
           playsInline
           className="absolute w-full h-full object-cover hidden md:block"
-        />
+        >
+          <source src={desktopVideo} type="video/mp4" />
+        </video>
       </a>
       {/* Background video for mobile screens */}
       <a href="/collections/allproducts" className="absolute w-full h-full">
@@ -25,7 +27,9 @@ export const Home = () => {
           muted
           playsInline
           className="absolute w-full h-full object-cover block md:hidden"
-        />
+        >
+          <source src={mobileVideo} type="video/mp4" />
+        </video>
       </a>
     </div>
   );
