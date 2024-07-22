d8eb94e9fe56128d0e2f95692bfb049931e9d029
AlanSabuv8 <alansabuv8@gmail.com>
2024-07-22 11:03:23 +0530

    update3

--- src/Components/Fotter.jsx
+++ src/Components/Fotter.jsx
@@ -4,7 +4,7 @@ export const Fotter = () => {
   return (
     <footer className="bg-black text-white mt-[0.5px]">
       <div className="container mx-auto">
-        <div className="flex flex-col text-xs items-center text-white w-full">
+        <div className="flex flex-col text-xs font-medium items-center text-white w-full">
           <div className="w-full justify-center">
             <div className="flex flex-wrap justify-center gap-x-8 gap-y-8 p-6 pt-8">
               <a href="/" className="hover:underline">HELP</a>
@@ -20,6 +20,11 @@ export const Fotter = () => {
               <a href="/" className="hover:underline">INSTAGRAM</a>
               <a href="/" className="hover:underline">FACEBOOK</a>
               <a href="/" className="hover:underline">X</a>
+            </div>
+          </div>
+          <div className="w-full flex justify-center border-t-[0.5px] border-zinc-800 p-2 pb-6">
+            <div className="flex flex-wrap justify-center font-light text-[10px]">
+              <a href="/" className="">© 2024 DAWN STORE. ALL RIGHTS RESERVED.</a>
             </div>
           </div>
         </div>
